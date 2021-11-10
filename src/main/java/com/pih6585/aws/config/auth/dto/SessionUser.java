e237500a90298fdff0413a7acd45b271173dd6fb
pih6585 <pih6585@naver.com>
2021-11-10 12:02:51 +0900

    feat : aws 5장 google 로그인 연동

---
+++ src/main/java/com/pih6585/aws/config/auth/dto/SessionUser.java
@@ -0,0 +1,20 @@
+package com.pih6585.aws.config.auth.dto;
+
+import java.io.Serializable;
+
+import com.pih6585.aws.domain.user.User;
+
+import lombok.Getter;
+
+@Getter
+public class SessionUser implements Serializable {
+	private String name;
+	private String email;
+	private String picture;
+
+	public SessionUser(User user) {
+		this.name = user.getName();
+		this.email = user.getEmail();
+		this.picture = user.getPicture();
+	}
+}
--- src/main/java/com/pih6585/aws/web/IndexController.java
+++ src/main/java/com/pih6585/aws/web/IndexController.java
@@ -1,11 +1,14 @@
 package com.pih6585.aws.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.pih6585.aws.config.auth.dto.SessionUser;
 import com.pih6585.aws.service.posts.PostsService;
 import com.pih6585.aws.web.dto.PostsResponseDto;
 
@@ -15,12 +18,17 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class IndexController {
 
-	@Autowired
-	private PostsService postsService;
+	private final PostsService postsService;
+	private final HttpSession httpSession;
 
 	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("posts", postsService.findAllDesc());
+
+		SessionUser user = (SessionUser)httpSession.getAttribute("user");
+		if (user != null) {
+			model.addAttribute("userName", user.getName());
+		}
 		return "index";
 	}
 

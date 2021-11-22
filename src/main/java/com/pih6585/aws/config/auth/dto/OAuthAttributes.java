630d78e47ba42d59e27922e2f2b518a3261f36c4
pih6585 <pih6585@naver.com>
2021-11-22 15:20:07 +0900

    feat : 카카오 인증추가

--- src/main/java/com/pih6585/aws/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/pih6585/aws/config/auth/dto/OAuthAttributes.java
@@ -2,6 +2,9 @@ package com.pih6585.aws.config.auth.dto;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.pih6585.aws.domain.user.Role;
 import com.pih6585.aws.domain.user.User;
 
@@ -10,6 +13,8 @@ import lombok.Getter;
 
 @Getter
 public class OAuthAttributes {
+
+	private static final Logger logger = LoggerFactory.getLogger(OAuthAttributes.class);
 
 	private Map<String, Object> attributes;
 	private String nameAttributeKey;
@@ -29,19 +34,22 @@ public class OAuthAttributes {
 
 	public static OAuthAttributes of(String registrationId, String userNameAttributeName,
 		Map<String, Object> attributes) {
-		if("naver".equals(registrationId)) {
+		if ("naver".equals(registrationId)) {
 			return ofNaver("id", attributes);
+		}
+		if ("kakao".equals(registrationId)) {
+			return ofKakao("id", attributes);
 		}
 		return ofGoogle(userNameAttributeName, attributes);
 	}
 
 	private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
-		Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+		Map<String, Object> response = (Map<String, Object>)attributes.get("response");
 
 		return OAuthAttributes.builder()
-			.name((String) response.get("name"))
-			.email((String) response.get("email"))
-			.picture((String) response.get("profile_image"))
+			.name((String)response.get("name"))
+			.email((String)response.get("email"))
+			.picture((String)response.get("profile_image"))
 			.attributes(response)
 			.nameAttributeKey(userNameAttributeName)
 			.build();
@@ -52,6 +60,21 @@ public class OAuthAttributes {
 			.name((String)attributes.get("name"))
 			.email((String)attributes.get("email"))
 			.picture((String)attributes.get("picture"))
+			.attributes(attributes)
+			.nameAttributeKey(userNameAttributeName)
+			.build();
+	}
+
+	private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+		// kakao는 kakao_account에 유저정보가 있다. (email)
+		Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
+		// kakao_account안에 또 profile이라는 JSON객체가 있다. (nickname, profile_image)
+		Map<String, Object> kakaoProfile = (Map<String, Object>)kakaoAccount.get("profile");
+
+		return OAuthAttributes.builder()
+			.name((String)kakaoProfile.get("nickname"))
+			.email((String)kakaoAccount.get("email"))
+			.picture((String)kakaoProfile.get("profile_image_url"))
 			.attributes(attributes)
 			.nameAttributeKey(userNameAttributeName)
 			.build();
